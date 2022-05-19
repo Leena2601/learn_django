c43f727167efe838a706ad4c482c7309f2cd476d
Leena <leena.kranthi@gmail.com>
2022-05-19 18:30:51 +0530

    updating user profile

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from.models import Profile
 from pyexpat import model
 from django import forms
 from django.contrib.auth.models import User
@@ -9,3 +10,18 @@ class UserRegisterForm(UserCreationForm):
         class Meta:
             model = User
             fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms. EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    email = forms. EmailField()
+
+    class Meta:
+        model = Profile
+        fields = ['image']
